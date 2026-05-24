bdafeca23e8934a237a2fc84e4b89f6ea9548163
rjimenezdata <rubensusername@gmail.com>
2026-05-23 23:20:48 -0700

    WHO package

---
+++ R/load_data.R
@@ -0,0 +1,15 @@
+#' Loading in Measles Data from TidyTuesday
+#'
+#' @returns A list containing two dataframes that record measles incidence at different
+#' levels of time. The first, cases_month, contains monthly data at the country-year-month
+#' level (i.e. Algeria, January 2012). The second, cases_year, contains yearly data
+#' at the country-year level (i.e. Algeria, 2012). Together, these datasets include
+#' information for 194 countries across multiple years and months.
+#'
+#' @export
+#'
+#' @examples
+
+load_data <- function(){
+  list(cases_month = readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-24/cases_month.csv'),
+       cases_year = readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-24/cases_year.csv'))}
