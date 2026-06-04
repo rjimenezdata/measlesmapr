f0fa9bf3991c2198a23adf3dbeb00f16ddcba7bb
rjimenezdata <rubensusername@gmail.com>
2026-06-03 17:29:28 -0700

    update test cases

--- tests/testthat/test-plotCases.R
+++ tests/testthat/test-plotCases.R
@@ -1,5 +1,6 @@
 test_that("plot the Americas and Africa", {
   expect_s3_class(plotCases(c("Americas", "Africa")), "ggplot")
   expect_s3_class(plotCases("Western Pacific"), "ggplot")
+  expect_s3_class(plotCases("Western Pacific", legend = F), "ggplot")
   expect_error(plotCases("apple"))
 })
